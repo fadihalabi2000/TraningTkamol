8c48b4d7fc7944acdf41ea8742b0851d83daec99
Health <Health@DESKTOP-OIT3DB2>
2024-03-19 12:09:46 +0300

    biuld v1.0

---
+++ docs/assets/js/d.js
@@ -0,0 +1,21 @@
+
+// var listOfOptions = [];
+// document.addEventListener('DOMContentLoaded', function () {
+//     document.getElementById('add').addEventListener('click', function () {
+//       var listOfOptions = [];
+//       var checkboxInputs = document.querySelectorAll('div.card div input[type="checkbox"]');
+      
+//       checkboxInputs.forEach(function (checkbox) {
+//         if (checkbox.checked) {
+//           var option = { roomId: checkbox.id };
+//           listOfOptions.push(option);
+//         }
+//       });
+  
+//       console.log(listOfOptions);
+//       alert('ffff');
+//     });
+//   });
+//   function getListOfOptions() {
+//     return listOfOptions;
+//   }
